80241834bb04a2a9c5bf932b482b6262e6fc33e9
Kshitiz Shrestha <er.kshitizshrestha@gmail.com>
2021-04-13 17:40:54 +0545

    latest

--- resources/views/packages.blade.php
+++ resources/views/packages.blade.php
@@ -18,13 +18,16 @@
                     <div class="col-lg-4">
                         <div class="box">
                             <h3>{{ $item->title }}</h3>
-                            <span class="badge badge-primary">1 Month: Rs. {{ $item->one_month_price }}</span>
-                            <span class="badge badge-danger">3 Months: Rs. {{ $item->three_month_price }}</span>
-                            <span class="badge badge-info">6 Months: Rs. {{ $item->six_month_price }}</span>
-                            <span class="badge badge-success">1 Year: Rs. {{ $item->one_year_price }}</span>
-                            <div style="margin-bottom: 4px">
-                                {!! $item->description !!}
+                            <div class="d-flex justify-content-between">
+                                <h4 style="font-size: 40px"><sup>Rs</sup>{{ $item->one_month_price }}<span>1 month</span></h4 style="font-size: 40px">
+                                <h4 style="font-size: 40px"><sup>Rs</sup>{{ $item->three_month_price }}<span>3 months</span></h4>
                             </div>
+                            <div class="d-flex justify-content-between">
+                                <h4 style="font-size: 40px"><sup>Rs</sup>{{ $item->six_month_price }}<span>6 months</span></h4>
+                                <h4 style="font-size: 40px"><sup>Rs</sup>{{ $item->one_year_price }}<span>1 year</span></h4>
+                            </div>
+
+                            {!! $item->description !!}
                             <a href="{{ route('register') }}" class="buy-btn">Get Started</a>
                         </div>
                     </div>
